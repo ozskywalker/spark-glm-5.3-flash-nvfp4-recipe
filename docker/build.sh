9ee43a708a3beb5c7936eeeaa71a79d830b224e5
Luke Walker <luke@lwalker.me>
2026-08-27 11:26:55 -0400

    fix: use seq for build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for i in {1..9}; do docker build -f "Dockerfile.glm53-sm121-v$i" -t "radixark/vllm-glm53-flash:glm53-sm121-v$i" . || break; done
+for i in $(seq 1 9); do docker build -f "Dockerfile.glm53-sm121-v$i" -t "radixark/vllm-glm53-flash:glm53-sm121-v$i" . || break; done
